4880ea46301553f8e9b04814c98ec436063c9a89
Alexander Kachalov <kachalow77@gmail.com>
2019-12-14 18:31:51 +0300

    updated TestClass.java added

--- src/test/java/TestClass.java
+++ src/test/java/TestClass.java
@@ -1,28 +1,52 @@
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
+import java.time.LocalDate;
+
 public class TestClass {
-    SelenideElement form;
+    private SelenideElement form;
+    private LocalDate date = LocalDate.now();
+    private int day = date.getDayOfMonth();
 
     @Test
     void testFormIfValidateAllInputData() {
-        inputData("Волгоград", "25", "Василий", "+79035783484");
+        openForm();
+        inputCityName("Волгоград");
+        inputNameUser("Василий");
+        inputPhoneNumber("+79035783484");
+        inputData();
+        formClick();
         $("[data-test-id=notification]").waitUntil(visible, 15000);
     }
 
-
-    private void inputData (String city, String date, String name, String phone) {
+    private void openForm() {
         open("http://localhost:9999");
         form = $(By.className("App_appContainer__3jRx1"));
+    }
+
+    private void inputCityName (String city) {
         form.$("[data-test-id=city] input").setValue(city);
-        form.$("[data-test-id=date]").click();
-        form.$(By.xpath("//*[text()='" + date + "']")).click();
+    }
+
+    private void inputNameUser (String name) {
         form.$("[data-test-id=name] input").setValue(name);
+    }
+
+    private void inputPhoneNumber (String phone) {
         form.$("[data-test-id=phone] input").setValue(phone);
+    }
+
+    private void inputData () {
+        form.$("[data-test-id=date]").click();
+        form.$(By.xpath("//*[text()='" + (day + 5) + "']")).click();
+    }
+
+    private void formClick() {
         form.$("[data-test-id=agreement]").click();
         form.$$("button").find(exactText("Забронировать")).click();
     }
